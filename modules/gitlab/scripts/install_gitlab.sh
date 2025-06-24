233a6378b4e4aff2714fe90fd1dda3dc3cae5b96
Svetoslav Petrov <svetlyo.leo.petrov@gmail.com>
2025-06-25 01:48:28 +0300

    chore: update initial config for gitlab

--- modules/gitlab/scripts/install_gitlab.sh
+++ modules/gitlab/scripts/install_gitlab.sh
@@ -8,12 +8,15 @@ sudo apt install -y curl openssh-server ca-certificates tzdata perl jq
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
 sudo apt install gitlab-ee
 
-# gitlab config
+# Gitlab config
 # https://gitlab.com/gitlab-org/omnibus-gitlab/blob/master/files/gitlab-config-template/gitlab.rb.template?_gl=1%2a1x2reot%2a_ga%2aMjAxODc0NTkxNi4xNjc2NzE0NzE2%2a_ga_ENFH3X7M5Y%2aMTY3NzAxMzY3OC41LjEuMTY3NzAxNDcwMS4wLjAuMA
 cat <<EOF > /etc/gitlab/gitlab.rb
 ${GITLAB_CONFIG}
 EOF
 sudo gitlab-ctl reconfigure
+
+# Apply some security patches to default configurations
+sudo gitlab-rails runner "ApplicationSetting.last.update_attribute(:signup_enabled, false)"
 
 # Install GitLab Runner
 curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
@@ -23,7 +26,7 @@ sudo apt install -y gitlab-runner
 AUTHORIZATION_TOKEN=$(curl --request POST --header "Content-Type: application/json" --data "{ \
     \"grant_type\": \"password\", \
     \"username\": \"root\", \
-    \"password\": \"${GITLAB_ROOT_PASSWORD}\" \
+    \"password\": \"$GITLAB_ROOT_PASSWORD\" \
   }" http://127.0.0.1/oauth/token)
 
 RUNNER_TOKEN=$(curl -k -H "Authorization: Bearer $AUTHORIZATION_TOKEN" -X POST 'http://127.0.0.1/api/v4/user/runners?runner_type=instance_type' | jq .token)
